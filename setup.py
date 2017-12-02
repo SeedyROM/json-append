042f39091f629b2f38485ed0a10dfb96c5eced59
Zack Kollar <rallokkcaz@gmail.com>
2017-12-02 06:24:56 -0800

    initial commit!

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of json-append.
+# https://github.com/someuser/somepackage
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2017, Zack Kollar <zackkollar@gmail.com>
+
+from setuptools import setup, find_packages
+from json_append import __version__
+
+tests_require = [
+    'mock',
+    'nose',
+    'coverage',
+    'yanc',
+    'preggy',
+    'tox',
+    'ipdb',
+    'coveralls',
+    'sphinx',
+]
+
+setup(
+    name='json-append',
+    version=__version__,
+    description='Append key value pairs to json files from the a cli.',
+    long_description='''
+Append key value pairs to json files from the a cli.
+''',
+    keywords='json configuration automation',
+    author='Zack Kollar',
+    author_email='zackkollar@gmail.com',
+    url='https://github.com/someuser/somepackage',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=[
+        # add your dependencies here
+        # remember to use 'package-name>=x.y.z,<x.y+1.0' notation (this way you get bugfixes)
+    ],
+    extras_require={
+        'tests': tests_require,
+    },
+    entry_points={
+        'console_scripts': [
+            # add cli scripts here in this form:
+            # 'json-append=json_append.cli:main',
+        ],
+    },
+)
