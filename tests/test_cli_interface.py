b7666df9964a0d8c21a5620745b09a4a00448755
Zack Kollar <rallokkcaz@gmail.com>
2017-12-07 12:37:23 -0800

    Added default file headers to test scripts.

--- tests/test_cli_interface.py
+++ tests/test_cli_interface.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of json-append.
+# https://github.com/SeedyROM/json-append
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2017, Zack Kollar <zackkollar@gmail.com>
+
 import json
 import os
 import subprocess
